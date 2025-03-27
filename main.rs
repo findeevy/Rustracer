e6fcf4bba74e09cf6ca80af3779fc3071cbe0c49
findeevy <findeevy@gmail.com>
2025-03-27 15:41:00 -0500

    got ppm output working and made a gradient

--- main.rs
+++ main.rs
@@ -1,7 +1,9 @@
 use std::ops::{Add, Sub, Mul};
+use std::fs::File;
+use std::io::{self, Write};
 
-const HEIGHT: i32 = 1000;
-const WIDTH: i32 = 1000;
+const HEIGHT: usize = 1000;
+const WIDTH: usize = 1000;
 
 #[derive(Debug, Copy, Clone)]
 struct Vector3{
@@ -15,6 +17,10 @@ impl Vector3{
   //Initiate a new Vector3 of three floats.
   fn new(x:f32, y:f32, z:f32) -> Self {
     Vector3 {x, y, z}
+  }
+
+  fn tou8(self) -> [u8; 3] {
+      [self.x as u8, self.y as u8, self.z as u8]
   }
 
   //Compute the dot product.
@@ -63,12 +69,51 @@ impl Mul<f32> for Vector3{
 
 }
 
+//Divide two usizes and return a float.
+fn udiv(x: usize, y: usize) -> f32{
+    return (x as f32)/(y as f32);
+}
+
+//Write the framebuffer to a ppm file.
+fn framebuffer_to_ppm(width: usize, height: usize, framebuffer: &Vec<Vector3>) -> io::Result<()>{
+    //Open the PPM file.
+    let mut file = File::create("./out.ppm")?;
+
+    //Write the header for the PPM file (P6 format, width, height, max color value).
+    writeln!(file, "P6")?;
+    writeln!(file, "{} {}", width, height)?;
+    writeln!(file, "255")?;
+
+    //Write the pixel data.
+    for y in 0..height {
+        for x in 0..width {
+            //Scale between 0 to 255 and convert to u8.
+            let pixel_value = Vector3::new(framebuffer[x+y*WIDTH].x, framebuffer[x+y*WIDTH].y, framebuffer[x+y*WIDTH].z);
+            let clamped = Vector3::tou8(pixel_value * 255.0);
+            for i in clamped{
+              file.write_all(&[i])?;
+            }
+        }
+    }
+
+    Ok(())
+}
 
 fn render(){
+  let mut framebuffer: Vec<Vector3> = vec![Vector3::new(0.0, 0.0, 0.0); WIDTH * HEIGHT];
   
+  //Create a gradient of pixels.
+  for y in 0..HEIGHT{
+    for x in 0..WIDTH{
+      framebuffer[x+y*WIDTH] = Vector3::new(udiv(y, HEIGHT), 0.0, udiv(x, WIDTH));
+    }
+  }
+
+  //Write our gradient to a PPM.
+  framebuffer_to_ppm(WIDTH, HEIGHT, &framebuffer);
 }
 
 
 fn main(){
-
+  render()
 }
