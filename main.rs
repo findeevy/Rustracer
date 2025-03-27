6980745c38390b79caae300dc59873528f535a83
findeevy <findeevy@gmail.com>
2025-03-27 17:05:08 -0500

    Finished up sphere, added some vector3 functionality, still need to figure out cast_ray function)

--- main.rs
+++ main.rs
@@ -3,7 +3,36 @@ use std::fs::File;
 use std::io::{self, Write};
 
 const HEIGHT: usize = 1000;
-const WIDTH: usize = 1000;
+iconst WIDTH: usize = 1000;
+
+#[derive(Debug, Copy, Clone)]
+struct Sphere{
+  transform: Vector3,
+  radius: f32,
+}
+
+impl Sphere{
+
+  fn new(transform: Vector3, radius: f32) -> Self {
+    Sphere {transform, f32}
+  }
+  
+  fn ray_interesect(orgin: Vector3, direction: Vector3, distance: Vector3) -> bool{
+    let length = self.transform - origin;
+    let ray = length * direction;
+    let difference_of_squares = length*length - ray*ray;
+    if (difference_of_squares > self.radius){
+      return false;
+    }
+    let temp = (self.radius*self.radius - difference_of_squares).sqrt();
+    let point0 = ray - difference_of_squares;
+    let point1 = ray + difference_of_sqaures;
+    if (point0 < 0){
+      point0 = point1;
+    }
+    return !(point0 < 0);
+  }
+}
 
 #[derive(Debug, Copy, Clone)]
 struct Vector3{
@@ -27,6 +56,27 @@ impl Vector3{
   fn dot(&self, other: &Vector3) -> f32 {
     self.x * other.x + self.y * other.y + self.z * other.z
   }
+  
+  //Computes the length of the vector.
+  fn magnitude(&self) -> f32 {
+    (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+  }
+  
+  //Normalizes the vector.
+  fn normalize(&self) -> Vector3 {
+    let magnitude = self.magnitude();
+    if(magnitude == 0.0){
+      return Vector3 {x: 0.0, y: 0.0, z:0.0}
+    }
+
+    Vector3{
+      x: self.x/magnitude,
+      y: self.y/magnitude,
+      z: self.z/magnitude,
+    }
+  }
+  
+
 }
 
 //Vector3 addition functionality.
@@ -99,7 +149,9 @@ fn framebuffer_to_ppm(width: usize, height: usize, framebuffer: &Vec<Vector3>) -
     Ok(())
 }
 
-fn render(){
+fn cast_ray()
+
+fn render_test_gradient(){
   let mut framebuffer: Vec<Vector3> = vec![Vector3::new(0.0, 0.0, 0.0); WIDTH * HEIGHT];
   
   //Create a gradient of pixels.
@@ -110,9 +162,25 @@ fn render(){
   }
 
   //Write our gradient to a PPM.
-  framebuffer_to_ppm(WIDTH, HEIGHT, &framebuffer);
+  let _ = framebuffer_to_ppm(WIDTH, HEIGHT, &framebuffer);
 }
 
+fn render(){
+  let mut framebuffer: Vec<Vector3> = vec![Vector3::new(0.0, 0.0, 0.0); WIDTH * HEIGHT];
+  let fov: f32 = 90;
+  let sphere = Sphere::new(Vector3::new(1.0, 1.0, 2.0), 1.0);
+
+  for y in 0...HEIGHT{
+    for x in 0...WIDTH{
+      let transform_x = (udiv((2*(i+0.5)), (WIDTH - 1)))*tan(fov/2.0)*udiv(WIDTH, HEIGHT);
+      let transform_y = (udiv((2*(i+0.5)), (WIDTH - 1)))*tan(fov/2.0);
+      let direction = Vector3::new(transform_x, transform_y, -1.0).normalize();
+      framebuffer[x+y*WIDTH] = cast_ray(Vector3::new(0.0, 0.0, 0.0), direction, sphere);
+    }
+  }
+
+  let _ = framebuffer_to_ppm(WIDTH, HEIGHT, &framebuffer);
+}
 
 fn main(){
   render()
